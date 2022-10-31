650ca7e54f81badcd1e0474674605d4d79550e4c
nakashi10 <94515963+kaeeeeel@users.noreply.github.com>
2022-10-31 17:09:27 +0900

    kokokara

---
+++ src/main/java/com/example/demo/AttendanceListController.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class AttendanceListController {
+    @RequestMapping("/attendance_list")
+    String home(Model model){
+        return "attendance_list";
+    }
+}
---
+++ src/main/java/com/example/servingwebcontent/ServingWebApplication.java
@@ -0,0 +1,13 @@
+package com.example.servingwebcontent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServingWebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(com.example.servingwebcontent.ServingWebApplication.class, args);
+    }
+
+}
